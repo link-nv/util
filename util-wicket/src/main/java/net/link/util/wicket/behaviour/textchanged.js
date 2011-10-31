75d42aad4a33dae95af55671ca8ef8db08ecc689
Stein Desmet <stein.desmet@link-k.net>
2011-10-31 13:05:30 +0100

    Added an ajax behaviour for detecting changes in input text fields, while the changes are happening (as opposed to onchange event)

---
+++ util-wicket/src/main/java/net/link/util/wicket/behaviour/textchanged.js
@@ -0,0 +1,28 @@
+ /*
+  * Script to trigger Wicket if the content of a text field is changeing
+  * @author Stein Desmet
+  */
+
+ if (typeof(Wicket) == "undefined")
+ 	Wicket = { };
+
+ Wicket.TextChanged=function(elementId, callbackUrl, enableTimer){
+      var txtField = $('#' + elementId);
+      txtField.data('oldValue', txtField.val());
+      txtField.bind("propertychange keyup input paste cut", watchTextBox );
+    
+      if(enableTimer==true) {
+          setInterval(watchTextBox, 100);
+      }
+
+      function watchTextBox(){
+          // If value has changed...
+          if (txtField.data('oldValue') != txtField.val()) {
+               // Updated stored value
+               txtField.data('oldValue', txtField.val());
+               // Callback to wicket
+               var wcall = wicketAjaxGet(callbackUrl+ (callbackUrl.indexOf("?")>-1 ? "&" : "?") + "tval="+txtField.val(), function() { }, function() { });
+         }
+      }
+ }
+
