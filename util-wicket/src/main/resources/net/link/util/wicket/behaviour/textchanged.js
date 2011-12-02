8d72990bbb66071d2db9117e579e63c65cdba685
Stein Desmet <stein.desmet@link-k.net>
2011-12-02 15:16:55 +0100

    Once the text has been changed, disable this script to avoid multiple ajax calls
    Note: this caused issues with the safe-online-acceptance HtmlUnitDriver

--- util-wicket/src/main/resources/net/link/util/wicket/behaviour/textchanged.js
+++ util-wicket/src/main/resources/net/link/util/wicket/behaviour/textchanged.js
@@ -1,5 +1,5 @@
  /*
-  * Script to trigger Wicket if the content of a text field is changeing
+  * Script to trigger Wicket if the content of a text field is changing
   * @author Stein Desmet
   */
 
@@ -14,14 +14,18 @@
       txtField.bind("propertychange keyup input paste cut", watchTextBox );
     
       if(enableTimer==true) {
-          setInterval(watchTextBox, 100);
+          var timer = setInterval(watchTextBox, 100);
       }
 
       function watchTextBox(){
           // If value has changed...
           if (txtField.data('oldValue') != txtField.val()) {
                // Updated stored value
-               txtField.data('oldValue', txtField.val());
+               //txtField.data('oldValue', txtField.val());
+               txtField.unbind("propertychange keyup input paste cut", watchTextBox );
+               if(enableTimer==true) {
+                    clearInterval(timer);
+                }
                // Callback to wicket
                var wcall = wicketAjaxGet(callbackUrl+ (callbackUrl.indexOf("?")>-1 ? "&" : "?") + "tval="+txtField.val(), function() { }, function() { });
          }
