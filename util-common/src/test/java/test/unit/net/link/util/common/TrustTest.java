615c8cbc8ca04077f8ce081b52cfa02dade5e5ac
Wim Vandenhaute <wim.vandenhaute@gmail.com>
2013-12-09 18:21:08 +0100

    Fix: cyclic dependency between util-common/test

--- util-common/src/test/java/test/unit/net/link/util/common/TrustTest.java
+++ util-common/src/test/java/test/unit/net/link/util/common/TrustTest.java
@@ -5,11 +5,10 @@ import static org.junit.Assert.*;
 import be.fedict.trust.MemoryCertificateRepository;
 import be.fedict.trust.TrustValidator;
 import com.lyndir.lhunath.opal.system.logging.Logger;
-import java.security.cert.CertPathValidatorException;
-import java.security.cert.X509Certificate;
+import java.io.InputStream;
+import java.security.cert.*;
 import net.link.util.common.CertificateChain;
 import net.link.util.common.PublicKeyTrustLinker;
-import net.link.util.test.pkix.PkiTestUtils;
 import org.junit.Test;
 
 
@@ -22,12 +21,12 @@ public class TrustTest {
             throws Exception {
 
         // setup
-        X509Certificate docdataCert = PkiTestUtils.loadCertificate( TrustTest.class.getResourceAsStream( "/docdata.pem" ) );
-        X509Certificate geotrust1Cert = PkiTestUtils.loadCertificate( TrustTest.class.getResourceAsStream( "/geotrust1.pem" ) );
-        X509Certificate geotrust2Cert = PkiTestUtils.loadCertificate( TrustTest.class.getResourceAsStream( "/geotrust2.pem" ) );
+        X509Certificate docdataCert = loadCertificate( TrustTest.class.getResourceAsStream( "/docdata.pem" ) );
+        X509Certificate geotrust1Cert = loadCertificate( TrustTest.class.getResourceAsStream( "/geotrust1.pem" ) );
+        X509Certificate geotrust2Cert = loadCertificate( TrustTest.class.getResourceAsStream( "/geotrust2.pem" ) );
         CertificateChain chain = new CertificateChain( geotrust1Cert, docdataCert, geotrust2Cert );
 
-        X509Certificate trustedCertificate = PkiTestUtils.loadCertificate( TrustTest.class.getResourceAsStream( "/equifax.pem" ) );
+        X509Certificate trustedCertificate = loadCertificate( TrustTest.class.getResourceAsStream( "/equifax.pem" ) );
 
         // operate
         MemoryCertificateRepository certificateRepository = new MemoryCertificateRepository();
@@ -59,5 +58,12 @@ public class TrustTest {
             logger.err( e, e.getMessage() );
             fail();
         }
+    }
+
+    public static X509Certificate loadCertificate(InputStream inputStream)
+            throws CertificateException {
+
+        CertificateFactory certificateFactory = CertificateFactory.getInstance( "X.509" );
+        return (X509Certificate) certificateFactory.generateCertificate( inputStream );
     }
 }
